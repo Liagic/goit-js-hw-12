7d6eb89b7c97153e1d77f9da66ff242ddf198d5d
liagic <leon_max@i.ua>
2026-02-21 19:19:04 +0200

    ladded load more button, show and hid it method, and session storage

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const apiKey = '54728905-47b91fba438408292028d34aa';
-export default function getImagesByQuery(query) {
-  return axios
-    .get('', {
-      params: {
-        key: apiKey,
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-      },
-    })
-    .then(response => response.data)
-    .catch(error => error.message);
+export default async function getImagesByQuery(query, page) {
+  const response = await axios.get('', {
+    params: {
+      key: apiKey,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 15,
+      page,
+    },
+  });
+  return response.data;
 }
--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ import {
   createGallery,
   showLoader,
   hideLoader,
-  updateLightBox,
+  hideLoadButton,
+  showLoadButton,
 } from './js/render-functions';
 
 function handleErrorMessage(message = 'Something went wrong') {
@@ -21,17 +22,13 @@ function handleErrorMessage(message = 'Something went wrong') {
     backgroundColor: '#ef4040',
   });
 }
-hideLoader();
-const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  const query = form.elements['search-text'].value.toLowerCase().trim();
-  if (!query) return;
-  clearGallery();
+function loadButtonHandler() {
+  let query = sessionStorage.getItem('search-text');
   showLoader();
-  getImagesByQuery(query)
+  current_page += 1;
+  if (!query) return;
+  getImagesByQuery(query, current_page)
     .then(res => {
-      hideLoader();
       if (res.hits.length === 0) {
         return Promise.reject(
           new Error(
@@ -42,7 +39,55 @@ form.addEventListener('submit', event => {
       createGallery(res.hits);
     })
     .catch(error => {
-      hideLoader();
       handleErrorMessage(error.message);
+    })
+    .finally(() => {
+      hideLoader();
     });
+  if (current_page >= total_pages) {
+    handleErrorMessage(
+      `We're sorry, but you've reached the end of search results.`
+    );
+    hideLoadButton();
+    return;
+  }
+}
+const form = document.querySelector('.form');
+const more_button = document.querySelector('.load-more');
+let current_page = 1;
+let total_pages = 0;
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const query = form.elements['search-text'].value.toLowerCase().trim();
+  if (!query) return;
+  sessionStorage.setItem('search-text', query);
+  clearGallery();
+  showLoader();
+  current_page = 1;
+  getImagesByQuery(query, current_page)
+    .then(res => {
+      if (res.hits.length === 0) {
+        return Promise.reject(
+          new Error(
+            'Sorry, there are no images matching your search query. Please try again!'
+          )
+        );
+      }
+      total_pages = Math.ceil(res.totalHits / 15);
+      createGallery(res.hits);
+      if (total_pages == current_page) {
+        handleErrorMessage(
+          `We're sorry, but you've reached the end of search results.`
+        );
+        hideLoadButton();
+      } else {
+        showLoadButton();
+      }
+    })
+    .catch(error => {
+      handleErrorMessage(error.message);
+    })
+    .finally(() => hideLoader());
 });
+more_button.addEventListener('click', loadButtonHandler);
